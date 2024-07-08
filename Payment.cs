503817f7e5fd7df9eb7f8382a4a28cbdbdc443b2
KrisiKaneva <kristiyana.kaneva13@gmail.com>
2024-07-08 11:15:11 +0300

    Create Payment.cs

---
+++ Payment.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Zadanie_8
+{
+	public class Payment
+	{
+		private string paymentID;
+		private string orderID;
+		private DateTime paymentDate;
+		private string paymentMethod;
+
+		public Payment()
+		{
+		}
+	}
+}
