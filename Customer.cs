0a55a4a59f04a0213e64dda58d9d89f231f75396
YoanaNeycheva <jony.neycheva@gmail.com>
2024-07-08 11:29:45 +0300

    Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -4,86 +4,90 @@ namespace Zadanie_8
 	public class Customer
 	{
 		private string customerID;
-public string CustomerName
-{
-    get { return customerID; }
-    set
-	{
-		if (value == " ")
-		{
-			throw new NullReferenceException();
-		}
-		else
-		{
-			customerID = value;
-		}
-	}
-}
+		public string CustomerName
+        {
+            get { return customerID; }
+            set
+			{
+				if (value == " ")
+				{
+					throw new NullReferenceException();
+				}
+				else
+				{
+					customerID = value;
+				}
+			}
+        }
 
-private string fullName;
-public string FullName
-{
-    get { return fullName; }
-    set
-    {
-        if (value == " ")
+        private string fullName;
+		public string FullName
         {
-            throw new NullReferenceException();
+            get { return fullName; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    fullName = value;
+                }
+            }
         }
-        else
-        {
-            fullName = value;
-        }
-    }
-}
 
-private string email;
-public string Email
-{
-    get { return email; }
-    set
-    {
-        if (value == " ")
+        private string email;
+		public string Email
         {
-            throw new NullReferenceException();
+            get { return email; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    email = value;
+                }
+            }
         }
-        else
-        {
-            email = value;
-        }
-    }
-}
 
-private string phoneNumber;
-public string PhoneNumber
-{
-    get { return phoneNumber; }
-    set
-    {
-        if (value == " ")
+        private string phoneNumber;
+		public string PhoneNumber
         {
-            throw new NullReferenceException();
+            get { return phoneNumber; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    phoneNumber = value;
+                }
+            }
         }
-        else
-        {
-            phoneNumber = value;
-        }
-    }
-}
 
-private string shippingAddress;
-public string ShippingAddress
-{
-    get { return shippingAddress; }
-    set
-    {
-        if (value == " ")
+        private string shippingAddress;
+		public string ShippingAddress
         {
-            throw new NullReferenceException();
+            get { return shippingAddress; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    shippingAddress = value;
+                }
+            }
         }
-        else
-        {
-            shippingAddress = value;
-        }
+
+
     }
 }
