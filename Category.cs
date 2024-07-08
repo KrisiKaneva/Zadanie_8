d939c6c67021d93c9a3699abbe80d51fcd15f9d5
YoanaNeycheva <jony.neycheva@gmail.com>
2024-07-08 11:55:13 +0300

    Category.cs

--- Category.cs
+++ Category.cs
@@ -6,9 +6,58 @@ namespace Zadanie_8
 	public class Category
 	{
 		private string categoryID;
+		public string CategoryID
+		{
+			get { return categoryID; }
+			set
+			{
+				if (value == " ")
+				{
+					throw new NullReferenceException();
+				}
+				else
+				{
+					categoryID = value;
+				}
+			}
+		}
+
 		private string name;
-		private string description;
-		private List<Product> Products;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    description = value;
+                }
+            }
+        }
+
+        public List<Product> Products;
 
 
         public Category()
@@ -16,3 +65,4 @@ namespace Zadanie_8
 		}
 	}
 }
+
