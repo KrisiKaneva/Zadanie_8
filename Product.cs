fbb4fbc107666775ee17ffcb2c15b8fb27573a03
KrisiKaneva <kristiyana.kaneva13@gmail.com>
2024-07-08 10:44:51 +0300

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Zadanie_8
+{
+	public class Product
+	{
+		private string productID;
+		private string name;
+		private string category;
+		private decimal price;
+		private int stockQuantity;
+		private string description;
+		private bool isAvailable;
+
+		public Product(string productID, string name, string category, decimal price, int stockQuantity, string description, bool isAvaileble)
+		{
+			
+		}
+	}
+}
+
