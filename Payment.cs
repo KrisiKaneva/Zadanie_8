0823d7eaa0d9cc55f3f9f300de718e5fca023e2c
YoanaNeycheva <jony.neycheva@gmail.com>
2024-07-08 12:06:47 +0300

    Payment.cs

--- Payment.cs
+++ Payment.cs
@@ -4,12 +4,73 @@ namespace Zadanie_8
 	public class Payment
 	{
 		private string paymentID;
-		private string orderID;
-		private DateTime paymentDate;
-		private string paymentMethod;
+        public string PaymentID
+        {
+            get { return paymentID; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    paymentID = value;
+                }
+            }
+        }
+        private string orderID;
+        public string OrderID
+        {
+            get { return orderID; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    orderID = value;
+                }
+            }
+        }
+        private DateTime paymentDate;
+        public string PaymentDate
+        {
+            get { return paymentDate; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    paymentDate = value;
+                }
+            }
+        }
+        private string paymentMethod;
+        public string PaymentMethod
+        {
+            get { return paymentMethod; }
+            set
+            {
+                if (value != "Credit card" || value != "Debit card" || value != "PayPal" || value != "Bank transfer")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    paymentMethod = value;
+                }
+            }
+        }
 
-		public Payment()
+        public Payment()
 		{
 		}
 	}
 }
+
