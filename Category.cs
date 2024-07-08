a602a97fd0a9ebe6efa195899f05117484e5b8cf
KrisiKaneva <kristiyana.kaneva13@gmail.com>
2024-07-08 11:08:07 +0300

    Create Category.cs

---
+++ Category.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace Zadanie_8
+{
+	public class Category
+	{
+		private string categoryID;
+		private string name;
+		private string description;
+		private List<Product> Products;
+
+
+        public Category()
+		{
+		}
+	}
+}
