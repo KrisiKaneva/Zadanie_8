dfe919c4fb6aceeb5e4915e5b466582f5bb9fa66
YoanaNeycheva <jony.neycheva@gmail.com>
2024-07-08 11:59:16 +0300

    Order.cs

--- Order.cs
+++ Order.cs
@@ -4,11 +4,73 @@ namespace Zadanie_8
 	public class Order
 	{
 		private string orderID;
-		private string customerID;
-		private DateTime orderDate;
+        public string OrderID
+        {
+            get { return orderID; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    orderID = value;
+                }
+            }
+        }
+
+        private string customerID;
+        public string CustomerID
+        {
+            get { return customerID; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    customerID = value;
+                }
+            }
+        }
+        private DateTime orderDate;
+        public string OrderDate
+        {
+            get { return orderID; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    orderID = value;
+                }
+            }
+        }
+        private string location;
+        public string Location
+        {
+            get { return location; }
+            set
+            {
+                if (value == " ")
+                {
+                    throw new NullReferenceException();
+                }
+                else
+                {
+                    location = value;
+                }
+            }
+        }
 
 
-		public Order()
+        public Order()
 		{
 		}
 	}
