96f566ebbcc03634336ec4348d0fcbfcd3cde033
YoanaNeycheva <jony.neycheva@gmail.com>
2024-07-08 11:18:16 +0300

    Update Customer.cs

--- Customer.cs
+++ Customer.cs
@@ -4,13 +4,88 @@ namespace Zadanie_8
 	public class Customer
 	{
 		private string customerID;
-		private string fullName;
-		private string email;
-		private string phoneNumber;
-		private string shippingAddress;
-
-		public Customer()
+public string CustomerName
+{
+    get { return customerID; }
+    set
+	{
+		if (value == " ")
 		{
+			throw new NullReferenceException();
 		}
+		else
+		{
+			customerID = value;
+		}
+	}
+}
+
+private string fullName;
+public string FullName
+{
+    get { return fullName; }
+    set
+    {
+        if (value == " ")
+        {
+            throw new NullReferenceException();
+        }
+        else
+        {
+            fullName = value;
+        }
+    }
+}
+
+private string email;
+public string Email
+{
+    get { return email; }
+    set
+    {
+        if (value == " ")
+        {
+            throw new NullReferenceException();
+        }
+        else
+        {
+            email = value;
+        }
+    }
+}
+
+private string phoneNumber;
+public string PhoneNumber
+{
+    get { return phoneNumber; }
+    set
+    {
+        if (value == " ")
+        {
+            throw new NullReferenceException();
+        }
+        else
+        {
+            phoneNumber = value;
+        }
+    }
+}
+
+private string shippingAddress;
+public string ShippingAddress
+{
+    get { return shippingAddress; }
+    set
+    {
+        if (value == " ")
+        {
+            throw new NullReferenceException();
+        }
+        else
+        {
+            shippingAddress = value;
+        }
+    }
+}
 	}
 }
