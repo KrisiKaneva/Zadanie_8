11e0e6d9ef586c23aadab21f062227e43d13ff05
KrisiKaneva <kristiyana.kaneva13@gmail.com>
2024-07-08 11:01:56 +0300

    Create Order.cs

---
+++ Order.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Zadanie_8
+{
+	public class Order
+	{
+		private string orderID;
+		private string customerID;
+		private DateTime orderDate;
+
+
+		public Order()
+		{
+		}
+	}
+}
